ce5d5dbc7060c1714b0cc80c3b6b28edf1114f18
VLONE <1760747128@qq.com>
2024-03-28 02:54:32 +0800

    update0.1

--- app/src/main/java/com/example/vlone/MainActivity.java
+++ app/src/main/java/com/example/vlone/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.vlone;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+EditText A,Code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
+        Button LogIn=findViewById(R.id.ButtonLogIn);
+
+
+        EditText Code=findViewById(R.id.editTextCODE);
+
+
+    }
+
+    public void getCount(View view) {
+        A=findViewById(R.id.EditTextCOUNT);
+      A.getText().toString();
+       String Cot=A.getText().toString();
+      Log.e("leg","COUNT:"+Cot);
+
     }
 }
